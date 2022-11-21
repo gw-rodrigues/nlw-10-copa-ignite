9374accf134d42c26b486a85fe8ea36970d8e667
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-21 15:27:44 +0100

    feat: fastify route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,17 @@
  *  npm i typescript -D
  *  npm i fastify
  *  npm i tsx -D       // Ajuda na compilação do código typescript para javascript e executa o ficheiro convertido,
- *  evita usar vários, como "npx tsc " e "node src/server.js" - depois so adicionamos no scripts(package.json) - "dev": "tsx src/server"
+ *  evita usar vários, como "npx tsc " e "node src/server.js" - depois so adicionamos no scripts(package.json) - "dev": "tsx watch src/server"
  *  basta no terminar usar "npm run dev"
+ */
+
+/**
+ * npm i prisma -D -> interface
+ * npm i @prima/client -> conexão com prisma
+ * npx prisma init --datasource-provider SQLite -> usar SQLite database, por padrão é PostSQL
+ *
+ * .env -> contém local da database
+ * schema.prisma -> podemos criar as tabelas/models (continuação código)
  */
 
 /**
@@ -15,8 +24,12 @@ import Fastify from 'fastify'
 /** Alguns frameworks usam nome da função como bootstrap - primeira coisa ser executada na app */
 async function bootstrap() {
   /** agora vamos criar nosso servidor */
-
   const fastify = Fastify({ logger: true })
+
+  /** Adicionar as nossa rotas */
+  fastify.get('/pools/count', () => {
+    return { count: 0 }
+  })
 
   await fastify.listen({ port: 3333 })
 }
