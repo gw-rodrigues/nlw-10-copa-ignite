eb1d7fe98deb88ec2a16715596e2d2d1a47506b9
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-22 16:12:26 +0100

    feat: first server request

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>hello world</h1>
+interface HomeProps {
+  count: number
+}
+export default function Home(props: HomeProps) {
+  return <h1>count - {props.count}</h1>
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  console.log(data)
+
+  return { props: { count: data.count } }
 }
