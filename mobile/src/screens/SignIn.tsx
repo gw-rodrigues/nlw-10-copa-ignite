b8845dece407e89be95257bbfc5b03b60b14f843
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-28 11:37:01 +0100

    feat: import useAuth signIn function

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -18,8 +18,11 @@ import { Fontisto } from '@expo/vector-icons'
 
 import Logo from '../assets/logo.svg'
 import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
 
 export function SignIn() {
+  const { signIn } = useAuth()
+
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
@@ -27,7 +30,8 @@ export function SignIn() {
         title="ENTER WITH GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         type="SECONDARY"
-        mt={12 }
+        mt={12}
+        onPress={signIn}
       />
       <Text color="white" textAlign="center" mt={4}>
         We do not use any information other than {'\n'} your email to create
