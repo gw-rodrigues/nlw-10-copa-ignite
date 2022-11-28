7162a9dedf3491d850bd31a3b4ca1c854604e630
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-28 09:52:31 +0100

    style: bg-color

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -15,7 +15,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body>
+      <body className="bg-[#121214]">
         <Main />
         <NextScript />
       </body>
