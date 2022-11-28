4774c95bd72daf5e975b8a790e2e843e0b3fc8f3
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-28 09:31:33 +0100

    feat: importing fonts

---
+++ web/src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+/**
+ * No nextjs é obrigatório usar o modelo CamelCase para todas as propriedades do elementos, igual no link das font google.
+ * !* nota: no crossOrigin é obrigatório definir um valor em branco - crossOrigin=""
+ */
