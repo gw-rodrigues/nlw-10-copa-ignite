3faffd7a98966e8ca9253b6c1099a6e96b950294
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-11-29 10:59:33 +0100

    feat: auth with google

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,5 +1,8 @@
 import { Center, Icon, Text } from 'native-base'
 import { Fontisto } from '@expo/vector-icons'
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
 
 /**
  * Para usar svg no react-native precisamos instalar:
@@ -16,9 +19,34 @@ import { Fontisto } from '@expo/vector-icons'
  * -> código da tipagem está no link acima.
  */
 
-import Logo from '../assets/logo.svg'
-import { Button } from '../components/Button'
-import { useAuth } from '../hooks/useAuth'
+/**
+ * Authentication with google
+ *
+ * Para fazer autenticação com google precisamos:
+ * -> fazer registo da nossa app no google - https://console.cloud.google.com/
+ *    -> criar novo projeto (só nome) - selecionar projeto
+ *    -> menu -> api & serviços -> tela de permissão OAuth
+ *        -> Externo -> precisa (nome, email, dados desenvolvedor)
+ *        -> Escopo -> adicionar escopo (email, user info) *quais info queremos do user
+ *        -> Usuário de teste -> nao
+ *        -> voltar para painel
+ *        -> PUBLICAR APLICATIVO
+ *
+ *    -> menu -> api & serviços -> credenciais
+ *    -> criar credencial -> ID do Cliente do OAuth
+ *    -> tipo aplicativo -> aplicativo da web
+ *    -> URIs (Authorized JavaScript origins): https://auth.expo.io
+ *    -> Authorized redirect URIs: https://auth.expo.io/@your-expo-username/your-project-slug (há forma automática de obter esse uri)
+ *      -> import * as AuthSession from 'expo-auth-session'
+ *      -> console.log(AuthSession.makeRedirectUri({ useProxy: true }))
+ *      -> no console: https://auth.expo.io/@gw-rodrigues/mobile
+ *      -> !!! caso nao aparecer precisa registar no https://expo.dev, depois terminar "npx expo login" e "npx expo whoami"
+ *
+ * -> instalar: npx expo install expo-auth-session expo-random  (gere a troca de tokens)
+ * -> instalar: npx expo install expo-web-browser   (abrir browser para user fazer login)
+ * -> criar um schema para receber o token após o processo autenticação, validado, fazendo link de redirecionamento:
+ *    -> app.json : "scheme": "nlwcopamobile",
+ */
 
 export function SignIn() {
   const { signIn } = useAuth()
