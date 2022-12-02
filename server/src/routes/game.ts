3e0d0b7d0debc65c38dd89f0344e28027d419393
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-02 10:57:09 +0100

    feat: returning games and guess of the game

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -6,6 +6,50 @@
  */
 
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { prisma } from '../lib/prisma'
+import { authenticate } from '../plugins/authenticate'
 
-export async function gameRoutes(fastify: FastifyInstance) {}
+export async function gameRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    '/polls/:id/games',
+    { onRequest: [authenticate] },
+    async (request) => {
+      const getPollsParams = z.object({
+        id: z.string(),
+      })
+
+      const { id } = getPollsParams.parse(request.params)
+
+      /**
+       * Além de buscar os games, vamos retorna se o user ja fez palpites para este jogo
+       */
+      const games = await prisma.game.findMany({
+        orderBy: {
+          date: 'desc',
+        },
+        include: {
+          guesses: {
+            where: { participant: { userId: request.user.sub, pollId: id } },
+          },
+        },
+      })
+
+      /**
+       * Como game pode ter vários palpites, mas o 1 user so pode fazer 1 palpite por jogo, fazendo a query acima
+       * retorna um "[]", e nao queremos, iremos verificar se existe um palpite, do user, criar campo "guess"
+       * e retorna o primeiro guess, caso existir, se nao null.
+       * ! definindo o campo "guesses" = undefined - irá fazer com este campo sumir/apagado.
+       */
+      return {
+        games: games.map((game) => {
+          return {
+            ...game,
+            guess: game.guesses.length > 0 ? game.guesses[0] : null,
+            guesses: undefined,
+          }
+        }),
+      }
+    },
+  )
+}
