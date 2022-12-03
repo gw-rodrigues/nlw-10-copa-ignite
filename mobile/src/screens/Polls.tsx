3b949fd68bb6d9cc29ffd10926e5727b5c117488
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-03 10:22:54 +0100

    feat: button navigate to go to find my bets

--- mobile/src/screens/Polls.tsx
+++ mobile/src/screens/Polls.tsx
@@ -2,8 +2,14 @@ import { Icon, VStack } from 'native-base'
 import { Octicons } from '@expo/vector-icons'
 import { Button } from '../components/Button'
 import { Header } from '../components/Header'
+import { useNavigation } from '@react-navigation/native'
 
 export function Polls() {
+  /**
+   * Iremos usar o navigation para navegar através do Button "search by code" para outro screen.
+   */
+  const { navigate } = useNavigation()
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="MY BETS" />
@@ -21,6 +27,7 @@ export function Polls() {
           leftIcon={
             <Icon as={Octicons} name="search" color="black" size="md" />
           }
+          onPress={() => navigate('find')}
         />
       </VStack>
     </VStack>
