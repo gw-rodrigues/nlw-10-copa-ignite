a9adceb6a769f54a0455b6f0a30aa981258e5f82
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-03 10:22:33 +0100

    feat: button method to go back to polls

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,38 +1,55 @@
-import { Text, HStack, Box } from 'native-base';
-import { CaretLeft, Export } from 'phosphor-react-native';
+import { useNavigation } from '@react-navigation/native'
+import { Text, HStack, Box } from 'native-base'
+import { CaretLeft, Export } from 'phosphor-react-native'
 
-import { ButtonIcon } from './ButtonIcon';
+import { ButtonIcon } from './ButtonIcon'
 
 interface Props {
-  title: string;
-  showBackButton?: boolean;
-  showShareButton?: boolean;
+  title: string
+  showBackButton?: boolean
+  showShareButton?: boolean
 }
 
-export function Header({ title, showBackButton = false, showShareButton = false }: Props) {
-  const EmptyBoxSpace = () => (<Box w={6} h={6} />);
+export function Header({
+  title,
+  showBackButton = false,
+  showShareButton = false,
+}: Props) {
+  /**
+   * Iremos usar o navigation para voltar para o screen anterior.
+   * -> existe a opção ".goBack()", mas volta para tela inicial, é queremos voltar para as polls (bets)
+   */
+  const { navigate } = useNavigation()
+
+  const EmptyBoxSpace = () => <Box w={6} h={6} />
 
   return (
-    <HStack w="full" h={24} bgColor="gray.800" alignItems="flex-end" pb={5} px={5}>
+    <HStack
+      w="full"
+      h={24}
+      bgColor="gray.800"
+      alignItems="flex-end"
+      pb={5}
+      px={5}
+    >
       <HStack w="full" alignItems="center" justifyContent="space-between">
-        {
-          showBackButton
-            ? <ButtonIcon icon={CaretLeft} />
-            : <EmptyBoxSpace />
-        }
+        {showBackButton ? (
+          <ButtonIcon icon={CaretLeft} onPress={() => navigate('polls')} />
+        ) : (
+          <EmptyBoxSpace />
+        )}
 
-        <Text color="white" fontFamily="medium" fontSize="md" textAlign="center">
+        <Text
+          color="white"
+          fontFamily="medium"
+          fontSize="md"
+          textAlign="center"
+        >
           {title}
         </Text>
 
-        {
-          showShareButton
-            ?
-            <ButtonIcon icon={Export} />
-            :
-            <EmptyBoxSpace />
-        }
+        {showShareButton ? <ButtonIcon icon={Export} /> : <EmptyBoxSpace />}
       </HStack>
     </HStack>
-  );
+  )
 }
