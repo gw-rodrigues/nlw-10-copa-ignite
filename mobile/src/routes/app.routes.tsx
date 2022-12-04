1cc06a3bcc201c8473948e73bc20633643738698
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-04 14:43:10 +0100

    feat: add bet details rote

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import { PlusCircle, SoccerBall } from 'phosphor-react-native'
  * Platform - serve para identificar qual ambiente no App está a rodar (OS, Android, Etc...)
  */
 import { Platform } from 'react-native'
+import { Details } from '../screens/Details'
 import { Find } from '../screens/Find'
 import { New } from '../screens/New'
 import { Polls } from '../screens/Polls'
@@ -93,6 +94,13 @@ export function AppRoutes() {
       <Screen
         name="find"
         component={Find}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
+      <Screen
+        name="details"
+        component={Details}
         options={{
           tabBarButton: () => null,
         }}
