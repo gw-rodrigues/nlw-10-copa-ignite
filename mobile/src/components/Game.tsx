269299be800d6e921f7157f1f9e108f94ad2d8b5
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-05 15:07:49 +0100

    fix: translation

--- mobile/src/components/Game.tsx
+++ mobile/src/components/Game.tsx
@@ -1,34 +1,39 @@
-import { Button, HStack, Text, useTheme, VStack } from 'native-base';
-import { X, Check } from 'phosphor-react-native';
-import { getName } from 'country-list';
+import { Button, HStack, Text, useTheme, VStack } from 'native-base'
+import { X, Check } from 'phosphor-react-native'
+import { getName } from 'country-list'
 
-import { Team } from './Team';
+import { Team } from './Team'
 
 interface GuessProps {
-  id: string;
-  gameId: string;
-  createdAt: string;
-  participantId: string;
-  firstTeamPoints: number;
-  secondTeamPoints: number;
+  id: string
+  gameId: string
+  createdAt: string
+  participantId: string
+  firstTeamPoints: number
+  secondTeamPoints: number
 }
 
 export interface GameProps {
-  id: string;
-  firstTeamCountryCode: string;
-  secondTeamCountryCode: string;
-  guess: null | GuessProps;
-};
+  id: string
+  firstTeamCountryCode: string
+  secondTeamCountryCode: string
+  guess: null | GuessProps
+}
 
 interface Props {
-  data: GameProps;
-  onGuessConfirm: () => void;
-  setFirstTeamPoints: (value: string) => void;
-  setSecondTeamPoints: (value: string) => void;
-};
+  data: GameProps
+  onGuessConfirm: () => void
+  setFirstTeamPoints: (value: string) => void
+  setSecondTeamPoints: (value: string) => void
+}
 
-export function Game({ data, setFirstTeamPoints, setSecondTeamPoints, onGuessConfirm }: Props) {
-  const { colors, sizes } = useTheme();
+export function Game({
+  data,
+  setFirstTeamPoints,
+  setSecondTeamPoints,
+  onGuessConfirm,
+}: Props) {
+  const { colors, sizes } = useTheme()
 
   return (
     <VStack
@@ -42,14 +47,20 @@ export function Game({ data, setFirstTeamPoints, setSecondTeamPoints, onGuessCon
       p={4}
     >
       <Text color="gray.100" fontFamily="heading" fontSize="sm">
-        {getName(data.firstTeamCountryCode)} vs. {getName(data.secondTeamCountryCode)}
+        {getName(data.firstTeamCountryCode)} vs.{' '}
+        {getName(data.secondTeamCountryCode)}
       </Text>
 
       <Text color="gray.200" fontSize="xs">
         22 de Novembro de 2022 às 16:00h
       </Text>
 
-      <HStack mt={4} w="full" justifyContent="space-between" alignItems="center">
+      <HStack
+        mt={4}
+        w="full"
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Team
           code={data.firstTeamCountryCode}
           position="right"
@@ -65,18 +76,23 @@ export function Game({ data, setFirstTeamPoints, setSecondTeamPoints, onGuessCon
         />
       </HStack>
 
-      {
-        !data.guess &&
-        <Button size="xs" w="full" bgColor="green.500" mt={4} onPress={onGuessConfirm}>
+      {!data.guess && (
+        <Button
+          size="xs"
+          w="full"
+          bgColor="green.500"
+          mt={4}
+          onPress={onGuessConfirm}
+        >
           <HStack alignItems="center">
             <Text color="white" fontSize="xs" fontFamily="heading" mr={3}>
-              CONFIRMAR PALPITE
+              CONFIRM GUESS
             </Text>
 
             <Check color={colors.white} size={sizes[4]} />
           </HStack>
         </Button>
-      }
+      )}
     </VStack>
-  );
+  )
 }
