71dd2ae64acda213e71329d70427cbbb1fa1706e
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-05 16:19:54 +0100

    feat: receiving code, loading empty component

--- mobile/src/components/Guesses.tsx
+++ mobile/src/components/Guesses.tsx
@@ -1,14 +1,16 @@
 import { Box, FlatList, useToast } from 'native-base'
 import { useEffect, useState } from 'react'
 import { api } from '../services/api'
+import { EmptyMyPollList } from './EmptyMyPollList'
 import { Game, GameProps } from './Game'
 import { Loading } from './Loading'
 
 interface Props {
   poolId: string
+  code: string
 }
 
-export function Guesses({ poolId }: Props) {
+export function Guesses({ poolId, code }: Props) {
   const [games, setGames] = useState<GameProps[]>([])
   const [isLoading, setIsLoading] = useState(true)
   const [firstTeamPoints, setFirstTeamPoints] = useState('')
@@ -86,6 +88,7 @@ export function Guesses({ poolId }: Props) {
         />
       )}
       _contentContainerStyle={{ pb: 32 }}
+      ListEmptyComponent={() => <EmptyMyPollList code={code} />}
     />
   )
 }
