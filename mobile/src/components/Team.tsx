0b62c641d96eda26f4e356fb0bcdce896e8ff835
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-06 17:32:28 +0100

    feat: passing input value and disable option

--- mobile/src/components/Team.tsx
+++ mobile/src/components/Team.tsx
@@ -1,18 +1,28 @@
-import { HStack } from 'native-base';
-import CountryFlag from "react-native-country-flag";
+import { HStack } from 'native-base'
+import CountryFlag from 'react-native-country-flag'
 
-import { Input } from './Input';
+import { Input } from './Input'
 
 interface Props {
-  code: string;
-  position: 'left' | 'right';
-  onChangeText: (value: string) => void;
+  code: string
+  position: 'left' | 'right'
+  onChangeText: (value: string) => void
+  inputDisabled?: boolean
+  inputValue?: number
 }
 
-export function Team({ code, position, onChangeText }: Props) {
+export function Team({
+  code,
+  position,
+  onChangeText,
+  inputDisabled = false,
+  inputValue = 0,
+}: Props) {
   return (
     <HStack alignItems="center">
-      {position === 'left' && <CountryFlag isoCode={code} size={25} style={{ marginRight: 12 }} />}
+      {position === 'left' && (
+        <CountryFlag isoCode={code} size={25} style={{ marginRight: 12 }} />
+      )}
 
       <Input
         w={10}
@@ -21,9 +31,13 @@ export function Team({ code, position, onChangeText }: Props) {
         fontSize="xs"
         keyboardType="numeric"
         onChangeText={onChangeText}
+        isDisabled={inputDisabled}
+        value={String(inputValue)}
       />
 
-      {position === 'right' && <CountryFlag isoCode={code} size={25} style={{ marginLeft: 12 }} />}
+      {position === 'right' && (
+        <CountryFlag isoCode={code} size={25} style={{ marginLeft: 12 }} />
+      )}
     </HStack>
-  );
+  )
 }
