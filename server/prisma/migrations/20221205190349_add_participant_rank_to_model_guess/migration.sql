7f0f7229e020e69b6e79b149842e5e71d486711e
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-12-06 18:00:34 +0100

    Build: Update DB Schema - Add user points and rank

---
+++ server/prisma/migrations/20221205190349_add_participant_rank_to_model_guess/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Guess" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "firstTeamPoints" INTEGER NOT NULL,
+    "secondTeamPoints" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "participantPoints" INTEGER NOT NULL DEFAULT 0,
+    "participantRank" INTEGER NOT NULL DEFAULT 0,
+    "gameId" TEXT NOT NULL,
+    "participantId" TEXT NOT NULL,
+    CONSTRAINT "Guess_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Guess_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Guess" ("createdAt", "firstTeamPoints", "gameId", "id", "participantId", "participantPoints", "secondTeamPoints") SELECT "createdAt", "firstTeamPoints", "gameId", "id", "participantId", "participantPoints", "secondTeamPoints" FROM "Guess";
+DROP TABLE "Guess";
+ALTER TABLE "new_Guess" RENAME TO "Guess";
+CREATE UNIQUE INDEX "Guess_participantId_gameId_key" ON "Guess"("participantId", "gameId");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
